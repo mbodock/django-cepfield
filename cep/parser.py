8be51b858937c35e387302197c9a9a3cb36f7a02
Marcus Vinicius (Mbodock) <mbodock@gmail.com>
2016-02-22 16:57:34 -0300

    Adds parser
    Now the page returned is parsed to obtain the proper fields like:
    - bairro
    - cidade
    - logradouro
    - estado

---
+++ cep/parser.py
@@ -0,0 +1,45 @@
+# encoding: utf-8
+import re
+
+
+class Parser(object):
+    def __init__(self, content):
+        self.content = content
+        self.labels = None
+        self.contents = None
+
+    def get_data(self):
+        self.get_labels()
+        self.get_contents()
+
+        return self.format_data()
+
+    def format_data(self):
+        data = {}
+        for label, value in zip(self.labels, self.contents):
+            if 'localidade' in label:
+                cidade, estado = value.split('/')
+                data['estado'] = estado.strip()
+                data['cidade'] = cidade.strip()
+            elif 'logradouro' in label and ' - ' in value:
+                logradouro, complemento = value.split(' - ')
+                data['logradouro'] = logradouro.strip()
+                data['complemento'] = complemento.strip()
+            elif value == u'endereço':
+                data['endereco'] = data[label] = value.strip()
+            else:
+                data[label] = value.strip()
+
+        return data
+
+    def get_labels(self):
+        matches = re.findall(r'<span class="resposta">(.*?)<\/span>',
+                             self.content,
+                             re.M | re.I | re.S)
+        self.labels = [i.lower().strip(' :') for i in matches]
+
+    def get_contents(self):
+        matches = re.findall(r'<span class="respostadestaque">(.*?)<\/span>',
+                             self.content,
+                             re.M | re.I | re.S)
+        self.contents = [re.sub(r'\s+', ' ', i) for i in matches]
--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,8 @@
+# encoding: utf-8
 import os
 import mock
 import sys
+import requests
 
 test_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(test_dir, os.path.pardir))
@@ -15,15 +17,25 @@ call_command('migrate', '--run-syncdb')
 
 from cep.models import Cep
 from cep.forms import CepField
+from cep.parser import Parser
 
 
 class FakeRequest(object):
     def __init__(self, content):
-        self.content = content
+        self.content = content.decode('ISO-8859-1')
 
 
-def fake_request_success(*args, **kwargs):
+class FakeException(requests.RequestException):
+    pass
+
+
+def fake_request_success_brasilia(*args, **kwargs):
     with open('tests/responses/success.html', 'r') as f:
+        return FakeRequest(f.read())
+
+
+def fake_request_success_logradouro(*args, **kwargs):
+    with open('tests/responses/success-logradouro.html', 'r') as f:
         return FakeRequest(f.read())
 
 
@@ -33,7 +45,7 @@ def fake_request_fail(*args, **kwargs):
 
 
 def fake_request_error(*args, **kwargs):
-    raise Exception('Internet Down')
+    raise FakeException('Internet Down')
 
 
 class CepModelTestCase(TestCase):
@@ -63,7 +75,7 @@ class CepFormTestCase(TestCase):
             field.clean('71150-903')
             # field.clean('70150-903')  Cep do palacio da alvorada
 
-    @mock.patch('requests.post', mock.Mock(side_effect=fake_request_success))
+    @mock.patch('requests.post', mock.Mock(side_effect=fake_request_success_brasilia))
     def test_correctly_cep(self):
         field = CepField()
         self.assertEqual('70.150-903', field.clean('70.150-903'))
@@ -79,8 +91,60 @@ class CepFormTestCase(TestCase):
         with self.assertRaises(ValidationError):
             field.clean('70.150-903')
 
-    @mock.patch('requests.post', mock.Mock(side_effect=iter(fake_request_success, fake_request_error)))
+    @mock.patch('requests.post',
+                mock.Mock(side_effect=iter(fake_request_success_brasilia, fake_request_error)))
     def test_revalidade_saved_cep(self):
         field = CepField()
         field.clean('70.150-903')
         self.assertEqual('70.150-903', field.clean('70.150-903'))
+
+
+class ParserTestCase(TestCase):
+    def setUp(self):
+        self.data = {
+            'logradouro': 'Alameda Ezequiel Dias',
+            'bairro': 'Centro',
+            'cidade': 'Belo Horizonte',
+            'estado': 'MG',
+        }
+
+    def test_parser_gets_bairro(self):
+        response = fake_request_success_logradouro()
+        parser = Parser(response.content)
+        parsed_data = parser.get_data()
+        self.assertEqual(self.data['bairro'],
+                         parsed_data['bairro'])
+
+    def test_parser_gets_cidade(self):
+        response = fake_request_success_logradouro()
+        parser = Parser(response.content)
+        parsed_data = parser.get_data()
+        self.assertEqual(self.data['cidade'],
+                         parsed_data['cidade'])
+
+    def test_parser_gets_bairro_com_acento(self):
+        data = {
+            'logradouro': 'SPP',
+            'bairro': u'Zona Cívico-Administrativa',
+            'cidade': u'Brasília',
+            'estado': 'DF',
+        }
+        response = fake_request_success_brasilia()
+        parser = Parser(response.content)
+        parsed_data = parser.get_data()
+        self.assertEqual(data['cidade'],
+                         parsed_data['cidade'])
+
+    def test_parser_gets_estado(self):
+        response = fake_request_success_logradouro()
+        parser = Parser(response.content)
+        parsed_data = parser.get_data()
+        self.assertEqual(self.data['estado'],
+                         parsed_data['estado'])
+
+    def test_parser_gets_logradouro(self):
+        response = fake_request_success_logradouro()
+        parser = Parser(response.content)
+        parsed_data = parser.get_data()
+        self.assertEqual(self.data['logradouro'],
+                         parsed_data['logradouro'])
