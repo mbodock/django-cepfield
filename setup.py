97d256de8539f2530fb9cef2cca3abd5f084aa88
Marcus Vinicius (Mbodock) <mbodock@gmail.com>
2016-02-23 17:33:37 -0300

    Version bump

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __description__ = 'Django CepField'
 __long_description__ = 'Auto fill address using brasizilian CEP number.'
 
